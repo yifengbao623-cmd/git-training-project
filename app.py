aaee92d11e42998192fff3c7a73ed5d63c3da644
Yifeng Bao <25300180069@m.fudan.edu.cn>
2026-05-06 13:54:53 +0800

    fix conflict

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-print("Hello World")
+print("Hello Logging System")
 
 def login():
     print("login success")
-=======
-print("Hello Logging System")
 
 def log():
     print("log something")
->>>>>>> 7fc3a16e65f35393c1f1fead553ab01399c7685c
